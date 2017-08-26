7552f13a15edafccb3f55f9785cfeb57e6c4f068
Ri Ram <eram1294@gmail.com>
2017-08-26 16:43:29 -0500

    some more changes

--- setup.r
+++ setup.r
@@ -9,13 +9,12 @@ source(file = "credentials.r")
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 eclipse <- searchTwitter("Eclipse2017 -filter:retweets", n = 285718, 
-                         since = "2017-08-20", until = "2017-08-22",
+                         since = "2017-08-20", until = "2017-08-23",
                          retryOnRateLimit = 120)
 eclipse.df <- twListToDF(eclipse)
 eclipse.df <- mutate(eclipse.df, create_date = date(created))
 
 eclipse_w_location <- filter(eclipse.df, !is.na(longitude)) %>%
-  filter(., create_date >= as.Date("2017-08-21")) %>%
   select(., id, longitude, latitude, created) %>%
   mutate(., longitude = as.double(longitude), latitude = as.double(latitude))
 
